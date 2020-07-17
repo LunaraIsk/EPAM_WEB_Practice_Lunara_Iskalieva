16a2e2be42c8626b15f11853e2e5e125b5ebda46
iskalievalunara0 <iskalievalunara0@gmail.com>
2020-07-17 06:03:28 +0300

    initial commit

---
+++ HTML_client/src/index.js
@@ -0,0 +1,9 @@
+
+import './js/common'
+import './js/function'
+//import './css/main.css'
+import './scss/main.scss'
+
+//import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+//import Vue from 'vue'
+//window.Vue =require('vue')
